e2edf3f8959c1f06a8093d85fbb886f98434b026
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-04-15 17:33:35 +0200

    [add] Some colors

--- index.php
+++ index.php
@@ -54,13 +54,14 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
-<body>
+<body class="bg-info">
 
   <div class="container py-5">
-    <form action="index.php" method="GET" class="d-flex justify-content-around">
+    <h1 class="text-center text-white mb-5">PHP Hotels</h1>
+    <form action="index.php" method="GET" class="d-flex justify-content-around fw-bold">
 
       <div>
-        <label for="isParking">Cerca per Parcheggio</label>
+        <label for="isParking" class="me-2">Cerca per Parcheggio</label>
         <select name="isParking" id="isParking">
           <option value="" selected>Disponibilità Parcheggio</option>
           <option value="1">Si</option>
@@ -69,18 +70,18 @@
       </div>
 
       <div>
-        <label for="rating">Cerca per Voto</label>  
+        <label for="rating" class="me-2">Cerca per Voto</label>  
         <select name="rating" id="rating">
           <option value="" selected>Qualsiasi Voto</option>
-          <option value="1">>1</option>
-          <option value="2">>2</option>
-          <option value="3">>3</option>
-          <option value="4">>4</option>
-          <option value="5">=5</option>
+          <option value="1">>1 &starf;</option>
+          <option value="2">>2 &starf;</option>
+          <option value="3">>3 &starf;</option>
+          <option value="4">>4 &starf;</option>
+          <option value="5">=5 &starf;</option>
         </select>
       </div>
 
-      <button type="submit">Invia</button>
+      <button type="submit" class="btn btn-light btn-outline-danger">Invia</button>
     </form>
   </div>
     
@@ -92,12 +93,12 @@
             $selectedParking = filter_var($isParking, FILTER_VALIDATE_BOOLEAN);
             if (($selectedParking === $hotel['parking'] || $isParking === '') && ($rating <= $hotel['vote'] || $rating === '')): ?>
         <div class="col mb-3">
-          <div class="card text-center">
+          <div class="card text-center rounded-4 bg-info-subtle">
             <div class="card-body">
               <h5 class="card-title"><?php echo $hotel['name'] ?></h5>
               <p class="card-text"><?php echo $hotel['description'] ?></p>
-              <p>Pracheggio: <?php $parking = $hotel['parking'] ? 'Si' : 'No'; echo $parking ?></p>
-              <p>Voto: <?php echo $hotel['vote'] ?></p>
+              <p>Parcheggio: <?php $parking = $hotel['parking'] ? 'Si' : 'No'; echo $parking ?></p>
+              <p>Voto: <?php echo $hotel['vote'] ?> &starf;</p>
               <p>Distanza dal centro: <?php echo $hotel['distance_to_center'] ?></p>
             </div>
           </div>
